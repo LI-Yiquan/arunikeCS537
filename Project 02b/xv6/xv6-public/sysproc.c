9ababc10fd5c94fb313fad9012977378ef364b40
Richie Zhou <65792694+arunike@users.noreply.github.com>
2022-10-27 09:22:57 -0500

    Added Project 2b

---
+++ Project 02b/xv6/xv6-public/sysproc.c
@@ -0,0 +1,146 @@
+#include "types.h"
+#include "x86.h"
+#include "defs.h"
+#include "date.h"
+#include "param.h"
+#include "memlayout.h"
+#include "mmu.h"
+#include "proc.h"
+#include "pstat.h"
+
+int
+sys_fork(void)
+{
+  return fork();
+}
+
+int
+sys_exit(void)
+{
+  exit();
+  return 0;  // not reached
+}
+
+int
+sys_wait(void)
+{
+  return wait();
+}
+
+int
+sys_kill(void)
+{
+  int pid;
+
+  if(argint(0, &pid) < 0)
+    return -1;
+  return kill(pid);
+}
+
+int
+sys_getpid(void)
+{
+  return myproc()->pid;
+}
+
+int
+sys_sbrk(void)
+{
+  int addr;
+  int n;
+
+  if(argint(0, &n) < 0)
+    return -1;
+  addr = myproc()->sz;
+  if(growproc(n) < 0)
+    return -1;
+  return addr;
+}
+
+int
+sys_sleep(void)
+{
+  int n;
+  uint ticks0;
+
+  if(argint(0, &n) < 0)
+    return -1;
+  acquire(&tickslock);
+  ticks0 = ticks;
+  while(ticks - ticks0 < n){
+    if(myproc()->killed){
+      release(&tickslock);
+      return -1;
+    }
+    sleep(&ticks, &tickslock);
+  }
+  release(&tickslock);
+  return 0;
+}
+
+// return how many clock tick interrupts have occurred
+// since start.
+int
+sys_uptime(void)
+{
+  uint xticks;
+
+  acquire(&tickslock);
+  xticks = ticks;
+  release(&tickslock);
+  return xticks;
+}
+
+int sys_getpinfo(void) {
+    struct pstat *ps;
+
+    if (argptr(0, (void*) & ps, sizeof(ps)) < 0) {
+        return -1;
+    }
+    if (ps == 0) {
+        return -1;
+    }
+
+    return getpinfo(ps);
+}
+
+int sys_settickets(void) {
+    int number;
+    
+    if (argint(0, &number) < 0) {
+      return -1;
+    }
+
+    return settickets(number); 
+}
+
+// Read only system call
+int sys_mprotect(void) {
+    // Variable declarations 
+    int addr;
+    int len;
+
+    if(argint(0, &addr) < 0) { // Get address
+        return -1;
+    }
+    if(argint(1, &len) < 0) { // Get length
+        return -1;
+    }
+
+    return mprotect((void *)addr,  len);
+}
+
+// Read and write only system call
+int sys_munprotect(void) {
+    int addr;
+    int len;
+
+    if(argint(0, &addr) < 0) { // Get the address of the page
+        return -1;
+    }
+    if(argint(1, &len) < 0) { // Get the length of the page
+        return -1;
+    }
+
+    return munprotect((void * )addr,  len);
+}
