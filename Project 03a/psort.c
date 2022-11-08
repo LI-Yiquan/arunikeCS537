eaee19a2fb6265eec9331867fca75e71fd4fe418
Richie Zhou <65792694+arunike@users.noreply.github.com>
2022-11-08 12:00:37 -0600

    Update psort.c

--- Project 03a/psort.c
+++ Project 03a/psort.c
@@ -8,118 +8,132 @@
 #include <sys/stat.h>
 #include <sys/mman.h>
 
-#define MAP_HUGE_1GB (30 << MAP_HUGE_SHIFT) // 1GB
+#define MAP_HUGE_1GB (30 << MAP_HUGE_SHIFT)
 
 // Global Variables
 int THREAD_MAX = 0;
 int THREAD_USED = 0;
+pthread_mutex_t merge_mutex;
 int multi_on;
 char* buf;
 int bufSize;
 
-// This function is used to compare two records
-int compareRecords(char* record, char* records) {
-    int key = *(int*) record; // Get the first 4 bytes of the first record
-    int keys = *(int*) records; // Get the first 4 bytes of the second record
+// Compares two records and returns 1 if the first record is greater than the second record
+int compareRecords(char* record, char* records) { 
+    // Variable initializations
+    int key = *(int*) record;
+    int keys = *(int*) records;
     
-    if(key <= keys) { // If the first record is smaller than the second record
+    if(key <= keys) { // If the first record is less than or equal to the second record
         return -1;
-    } else { // If the first record is larger than the second record
+    }else { // If the first record is greater than the second record
         return 1;
     }
+
+    return 0;
 }
 
-// This function is used to calculate the offset of a record
-int stride(int index) { 
+// Returns the stride of the index
+int stride(int index) {
     return index * 100;
 }
 
-// Merge two sorted arrays
+// Merges two sorted arrays
 void merge(int left, int mid, int right) {
-    // Variable initializations
+    // Variable declarations
     int i = 0;
     int j = 0;
     int k = left;
     int n1 = mid - left + 1;
     int n2 = right - mid;
 
-    char* Left = malloc((n1 * 100) * sizeof(char)); // Allocate memory for the first array
-    char* Right = malloc((n2 * 100) * sizeof(char)); // Allocate memory for the second array
+    // Array declarations
+    char* Left = malloc((n1 * 100) * sizeof(char));
+    char* Right = malloc((n2 * 100) * sizeof(char));
 
-    memcpy(Left, (buf + stride(left)), n1 * 100); // Copy the first array into the allocated memory
-    memcpy(Right, (buf + stride(mid + 1)), n2 * 100); // Copy the second array into the allocated memory
+    // Copy array 
+    memcpy(Left, (buf + stride(left)), n1 * 100);
+    memcpy(Right, (buf + stride(mid + 1)), n2 * 100);
 
-    while(i < n1 && j < n2) { // While there are still elements in both arrays
-        if(compareRecords((Left + stride(i)), (Right + stride(j))) < 0) {
+    while(i < n1 && j < n2) { // While the left and right arrays have not been fully traversed
+        if(compareRecords((Left + stride(i)), (Right + stride(j))) < 0) { // If the left array is less than the right array
             memcpy((buf + stride(k)), &Left[stride(i)], 100);
             i++;
-        } else { // If the first record is larger than the second record
+        } else { // If the right array is less than the left array
             memcpy((buf + stride(k)), &Right[stride(j)], 100);
             j++;
         }
         k++;
     }
-
-    while(i < n1) { // Copy the remaining elements of Left[], if there are any
+    while(i < n1) { // While the left array has not been fully traversed
         memcpy((buf + stride(k)), &Left[stride(i)], 100);
         i++;
         k++;
     }
-
-    free(Left); // Free cache
-
-    while(j < n2) { // Copy the remaining elements of Right[], if there are any
+    while(j < n2) { // While the right array has not been fully traversed
         memcpy((buf + stride(k)), &Right[stride(j)], 100);
         j++;
         k++;
     }
 
-    free(Right); // Free cache
-
+    free(Left);
+    free(Right);
 }
 
-// Merge sort function
+// Merge sort to be used by threads
 void* mergeSort(void* arg) {
-    // Variable initializations
+    // Variable initializations 
     int *args = (int*) arg;
     int left = args[0];
     int right = args[1];
     int mid = left + (right - left) / 2;
     int args1[2];
     int args2[2];
+    int flag_1 = 0;
+    int flag_2 = 0;
 
-    // Initialization array positions
+    // Array positions
     args1[0] = left;
     args1[1] = mid;
-    args2[0] = mid + 1;
+    args2[0] = mid+1;
     args2[1] = right;
 
-    if(left < right) { // If the left index is smaller than the right index
-        pthread_t new_thread_1, new_thread_2;
-        if(THREAD_USED < THREAD_MAX) { // If there are still threads available
+    if(left < right) { // If the left index is less than the right index
+        pthread_t new_thread_1, new_thread_2; // Create new threads
+        if(THREAD_USED < THREAD_MAX && multi_on == 1) { // If the number of threads used is less than the maximum number of threads and multi-threading is on
             THREAD_USED++;
             //printf("Thread num: %d\n",THREAD_USED);
+            flag_1 = 1;
             pthread_create(&new_thread_1, NULL, mergeSort, args1);
-            pthread_join(new_thread_1, NULL);
-            THREAD_USED--;
             //printf("Thread num: %d\n",THREAD_USED);
-        } else { // If there are no threads available
+        } else { // If multi-threading is off
             mergeSort(args1);
             //printf("Thread num: MAX\n");
         }
-        if(THREAD_USED < THREAD_MAX) { // If there are still threads available
+        if(THREAD_USED < THREAD_MAX && multi_on == 1) { // If the number of threads used is less than the maximum number of threads and multi-threading is on
             THREAD_USED++;
             //printf("Thread num: %d\n",THREAD_USED);
+            flag_2 = 1;
             pthread_create(&new_thread_2, NULL, mergeSort, args2);
-            pthread_join(new_thread_2, NULL);
-            THREAD_USED--;
             //printf("Thread num: %d\n",THREAD_USED);
-        } else { // If there are no threads available
+        } else { // If multi-threading is off
             mergeSort(args2);
             //printf("Thread num: MAX\n");
         }
-
-        merge(left, mid, right); // Merge the two sorted arrays
+        if(flag_1 == 1) { // If the first thread was created
+            pthread_join(new_thread_1, NULL);
+            THREAD_USED--;
+            flag_1 = 0;
+        }
+        if(flag_2 == 1) { // If the second thread was created
+            pthread_join(new_thread_2, NULL);
+            THREAD_USED--;
+            flag_2 = 0;
+        }
+        
+        pthread_mutex_lock(&merge_mutex); // Lock the mutex
+        merge(left, mid, right); // Merge the two arrays
+        pthread_mutex_unlock(&merge_mutex); // Unlock the mutex
     }
 
     return NULL;
@@ -127,7 +141,7 @@ void* mergeSort(void* arg) {
 
 // Main function
 int main(int argc, char *argv[]) {
-    struct stat st; // File status
+    struct stat st; // File status 
     int fd = open(argv[1], O_RDONLY, S_IRUSR | S_IWUSR); // Open the file
 
     if (fstat(fd, &st) == -1) { // If the file cannot be opened
@@ -142,27 +156,31 @@ int main(int argc, char *argv[]) {
         exit(0);
     }
 
-    buf = mmap(NULL, bufSize, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_NORESERVE, fd, 0); // Map the file into memory
-    int numRecords = bufSize / 100; // Get the number of records in the file
-    int arg[2]; // Argument array
+    buf = mmap(NULL, bufSize, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_NORESERVE, fd, 0); // Map the file to memory
+    int numRecords = bufSize / 100; // Get the number of records
+    int arg[2]; // Array declaration
 
     if(numRecords < 20000) { // If the number of records is less than 20000
         multi_on = 0;
     } else { // If the number of records is greater than 20000
         multi_on = 1;
     }
-    
-    THREAD_MAX = get_nprocs(); // Get the number of cores
-    THREAD_USED = 1; // Set the number of threads used to 1
-    arg[0] = 0; // Set the left index to 0
-    arg[1] = numRecords-1; // Set the right index to the number of records - 1
-    pthread_t pthread_main; // Create a new thread
 
-    pthread_create(&pthread_main, NULL, mergeSort, arg); // Create a new thread
-    pthread_join(pthread_main, NULL); // Wait for the thread to finish
+    THREAD_MAX = get_nprocs(); // Set max thread to number of processors
+    //THREAD_MAX = 8;
+    THREAD_USED = 1; // Set used thread to be 1
+
+    // Array declarations
+    arg[0] = 0;
+    arg[1] = numRecords - 1;
+
+    pthread_t pthread_main; // Create new thread
+    pthread_create(&pthread_main, NULL, mergeSort, arg); // Create new thread
+    pthread_join(pthread_main, NULL); // Join the thread
 
     //printf("Final_Thread num: %d\n",THREAD_USED);
-    // Variable initializations
+
+    // Variable declarations
     int file;
     int ret;
 
@@ -180,7 +198,7 @@ int main(int argc, char *argv[]) {
         exit(1);
     }
 
-    fsync(file); // Flush the file
+    fsync(file); // Sync the file
     close(file); // Close the file
 
     return 0;
